8eeba773d9df5a6866df1c6646cf387a65c394f7
Richard Glatter <rglatter.ieu2023@student.ie.edu>
2025-01-14 18:41:55 +0100

    last change so far

---
+++ logical_op.py
@@ -0,0 +1,7 @@
+print(2 or 3)
+print(2 and 3)
+print(2 or 3 or 4 or 5)
+print(2 and 3 and 0 and 5) # Cause 0 is a False value
+print(0 or None or 7 or 0.0) # 7 is the only True one
+
+#Generally the final output is what makes the chain true, basically the last number until the condition is satisfied
