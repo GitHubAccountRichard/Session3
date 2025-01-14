664aafd322e4ecbd3af597a00a52d83a83922fee
Richard Glatter <rglatter.ieu2023@student.ie.edu>
2025-01-14 18:28:06 +0100

    last change so far

---
+++ arithmetic_op.py
@@ -0,0 +1,7 @@
+print(2+3)
+print(2-3)
+print(2*3)
+print(4/2)
+print(4//2) #integer division
+print(9%5) #modulo or remainder from division
+print(2**3) #to the power
--- first_file.py
+++ first_file.py
@@ -2,3 +2,4 @@ print("This is another example")
 #print("I can do this all day")
 print(10+10*7)
 print("This is another example")
+
